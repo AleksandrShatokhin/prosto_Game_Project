8ca5859b0c1fb514b76f26b057068a307712c644
Aleksandr Shatokhin <al.shatokhin2010@yandex.ru>
2023-01-13 12:33:26 +0300

    delete room player from the scene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,9 +7,16 @@ public class GameController : MonoBehaviour
     private static GameController instance;
     public static GameController GetInstance() => instance;
 
+    [SerializeField] private GameObject firstWindow;
+
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        Instantiate(firstWindow, firstWindow.transform.position, firstWindow.transform.rotation);
     }
 
     public void SwitchWindow(GameObject currentWindow, GameObject newWindow)
