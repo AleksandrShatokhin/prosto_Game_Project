129ce9c0dc64091cf0459aacf3b39c709dfe3df6
merfields <54494980+merfields@users.noreply.github.com>
2023-01-18 17:07:20 +0300

    Added inventory screen + refactoring

--- Assets/Scripts/MapButton.cs
+++ Assets/Scripts/MapButton.cs
@@ -6,8 +6,6 @@ public class MapButton : MonoBehaviour
 {
 
     [SerializeField] private GameObject CityMap;
-    
-   
     public void MapClick()
     {
         Instantiate(CityMap, new Vector3(0, 0, 0), Quaternion.identity);
