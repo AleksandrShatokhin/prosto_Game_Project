5f7a159f46d3b420e7202ad0f47c636e5e995854
AleksandrShatokhin <al.shatokhin2010@yandex.ru>
2023-04-11 16:43:44 +0300

    added blur effect to pause window

---
+++ Assets/Scripts/Game/CameraScreenshot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScreenshot : MonoBehaviour
+{
+    public Sprite GetCreenSprite()
+    {
+        int cameraWidth = Camera.main.pixelWidth;
+        int cameraHeight = Camera.main.pixelHeight;
+        int cameraDepth = (int)Camera.main.transform.position.z;
+
+        RenderTexture renderTexture = new RenderTexture(cameraWidth, cameraHeight, cameraDepth);
+        Rect rect = new Rect(0, 0, cameraWidth, cameraHeight);
+        Texture2D texture2D = new Texture2D(cameraWidth, cameraHeight, TextureFormat.RGBA32, false);
+
+        Camera.main.targetTexture = renderTexture;
+        Camera.main.Render();
+
+        RenderTexture currentRenderTexture = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        texture2D.ReadPixels(rect, 0, 0);
+        texture2D.Apply();
+
+        Camera.main.targetTexture = null;
+        RenderTexture.active = currentRenderTexture;
+        Destroy(renderTexture);
+
+        Sprite screenSprite = Sprite.Create(texture2D, rect, Vector2.zero);
+
+        return screenSprite;
+    }
+}
--- Assets/Scripts/Game/PauseMenu.cs
+++ Assets/Scripts/Game/PauseMenu.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] private UnityEngine.UI.Image imageBack;
+
+    [SerializeField] private Shader blurShader;
+    private Material blurMaterial;
+
+    private void Start()
+    {
+        blurMaterial = new Material(blurShader);
+        imageBack.material = blurMaterial;
+    }
+
+    private void OnEnable()
+    {
+        Sprite spriteToBack = Camera.main.GetComponent<CameraScreenshot>().GetCreenSprite();
+        imageBack.sprite = spriteToBack;
+    }
+
     public void ExitGame()
     {
         Application.Quit();
