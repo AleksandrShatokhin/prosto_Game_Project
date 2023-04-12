2b5fd448dea1b2e098574e021b702734d9ba9fd9
AleksandrShatokhin <al.shatokhin2010@yandex.ru>
2023-04-12 20:30:25 +0300

    first prototype buttons and sliders in box_2

---
+++ Assets/Scripts/Box/BoxButtonSpriteSwitcher.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoxButtonSpriteSwitcher : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> spriteList;
+    [SerializeField] private int indicator;
+
+    private void Start()
+    {
+        SetFirstSprite();
+    }
+
+    public void ClickButton()
+    {
+        if (indicator == spriteList.Count - 1)
+        {
+            SetFirstSprite();
+        }
+        else
+        {
+            SetNewSprite();
+        }
+    }
+
+    private void SetFirstSprite()
+    {
+        indicator = 0;
+        this.GetComponent<Image>().sprite = spriteList[indicator];
+    }
+
+    private void SetNewSprite()
+    {
+        indicator += 1;
+        this.GetComponent<Image>().sprite = spriteList[indicator];
+    }
+}
