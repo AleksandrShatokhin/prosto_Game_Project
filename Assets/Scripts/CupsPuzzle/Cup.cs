e0ff39d2ad4f6c046817d3d06a1b09a98075af9c
merfields <54494980+merfields@users.noreply.github.com>
2023-05-08 19:23:38 +0300

    Configured cups puzzle

--- Assets/Scripts/CupsPuzzle/Cup.cs
+++ Assets/Scripts/CupsPuzzle/Cup.cs
@@ -63,6 +63,6 @@ public class Cup : MonoBehaviour, IClickable
 
     public void UpdateWaterLevelImage()
     {
-        waterImage.fillAmount = waterLevel / 1000 /*/ maxCapacity*/;
+        waterImage.fillAmount = waterLevel / maxCapacity;
     }
 }
