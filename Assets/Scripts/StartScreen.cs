acb5d27bc911875b0c2375e7e16e0bdcfd238a8d
Maxim Panchenko <mfire.panchenko@gmail.com>
2023-05-17 14:52:48 +0500

    Music added; Menu Music in Menu Scene

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class StartScreen : MonoBehaviour
 {
@@ -12,6 +13,11 @@ public class StartScreen : MonoBehaviour
     {
         buttonStart.onClick.AddListener(StartGame);
         buttonExit.onClick.AddListener(ExitGame);
+
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        AudioClip clip = (AudioClip)AssetDatabase.LoadAssetAtPath("Assets/Sound/Music/Menu.mp3", typeof(AudioClip));
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 
     private void StartGame()
