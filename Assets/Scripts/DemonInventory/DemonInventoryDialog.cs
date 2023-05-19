c202f71e7107a9007c711209b17d976b9e446ad7
merfields <54494980+merfields@users.noreply.github.com>
2023-05-20 00:26:49 +0300

    Bug fixes

--- Assets/Scripts/DemonInventory/DemonInventoryDialog.cs
+++ Assets/Scripts/DemonInventory/DemonInventoryDialog.cs
@@ -3,34 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
 public class DemonInventoryDialog : MonoBehaviour
 {
     [SerializeField] public PackCollection DialogPacks;
+
+    [SerializeField] private DemonInventory demonInventory;
+    [SerializeField] private Toggle[] demonButtons;
 
     private int _currentDialogPair = 0;
     private int _currentDialogPack = 0;
     public void OnAskDemonButtonClicked()
     {
-        try
+        if (demonInventory.caughtDemons.Count > 0 && IsAnyDemonButtonPressed())
         {
-            if (_currentDialogPair < DialogPacks.Packs[_currentDialogPack].DialogPairs.Count)
+            try
             {
-                GameObject textPairToActivate = DialogPacks.Packs[_currentDialogPack].DialogPairs[_currentDialogPair];
-                textPairToActivate.SetActive(true);
-                IEnumerator textFadeCoroutine = FadeIn(textPairToActivate);
-                StartCoroutine(textFadeCoroutine);
 
-                _currentDialogPair++;
-            }
-            else
-            {
-                DialogPacks.Packs[_currentDialogPack].gameObject.SetActive(false);
-                _currentDialogPack++;
-                if (_currentDialogPack < DialogPacks.Packs.Count)
+                if (_currentDialogPair < DialogPacks.Packs[_currentDialogPack].DialogPairs.Count)
                 {
-                    _currentDialogPair = 0;
-                    DialogPacks.Packs[_currentDialogPack].gameObject.SetActive(true);
-                    
                     GameObject textPairToActivate = DialogPacks.Packs[_currentDialogPack].DialogPairs[_currentDialogPair];
                     textPairToActivate.SetActive(true);
                     IEnumerator textFadeCoroutine = FadeIn(textPairToActivate);
@@ -40,15 +32,32 @@ public class DemonInventoryDialog : MonoBehaviour
                 }
                 else
                 {
-                    Debug.Log("Reached last message");
-                    _currentDialogPair--;
+                    DialogPacks.Packs[_currentDialogPack].gameObject.SetActive(false);
+                    _currentDialogPack++;
+                    if (_currentDialogPack < DialogPacks.Packs.Count)
+                    {
+                        _currentDialogPair = 0;
+                        DialogPacks.Packs[_currentDialogPack].gameObject.SetActive(true);
+
+                        GameObject textPairToActivate = DialogPacks.Packs[_currentDialogPack].DialogPairs[_currentDialogPair];
+                        textPairToActivate.SetActive(true);
+                        IEnumerator textFadeCoroutine = FadeIn(textPairToActivate);
+                        StartCoroutine(textFadeCoroutine);
+
+                        _currentDialogPair++;
+                    }
+                    else
+                    {
+                        Debug.Log("Reached last message");
+                        _currentDialogPair--;
+                    }
                 }
             }
-        }
-        catch (ArgumentOutOfRangeException exception)
-        {
-            Debug.Log("Reached last message");
-            _currentDialogPair--;
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Debug.Log("Reached last message");
+                _currentDialogPair--;
+            }
         }
     }
 
@@ -57,6 +66,13 @@ public class DemonInventoryDialog : MonoBehaviour
         float duration = 1f;
         float startTime = Time.time;
         TMP_Text[] texts = pair.GetComponentsInChildren<TMP_Text>();
+
+        foreach (TMP_Text text in texts)
+        {
+            Color clr = text.color;
+            Color newColor = new Color(clr.r, clr.g, clr.b, 0);
+            text.color = newColor;
+        }
         while (Time.time < startTime + duration)
         {
             foreach (TMP_Text text in texts)
@@ -73,5 +89,17 @@ public class DemonInventoryDialog : MonoBehaviour
     {
         _currentDialogPack = 0;
         _currentDialogPair = 0;
+    }
+
+    private bool IsAnyDemonButtonPressed()
+    {
+        foreach (var item in demonButtons)
+        {
+            if (item.isOn)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
